13a4262863c31832726337436b34819685d92eb8
Kimaru thagana <macbuntu@localhost.localdomain>
2019-07-05 21:25:01 +0300

    fix

--- visualize_network.py
+++ visualize_network.py
@@ -1,9 +1,19 @@
 import plotly
 from water_system import *
 from plotly.offline import download_plotlyjs, init_notebook_mode,  iplot, plot
-init_notebook_mode(connected=True)
+#init_notebook_mode(connected=True)
 
 
 #layout can be one of several, spectral, spring, fruchterman_reingold_layout among others
 pos=nx.fruchterman_reingold_layout(water_system)
 print(pos)# key is node index and value is an array of cordinates the layout algorithm has assigned each node
+
+Xs=[pos[k][0] for k in pos]
+Ys=[pos[k][1] for k in pos]
+trace_nodes=dict(type='scatter',
+                 x=Xs,
+                 y=Ys,
+                 mode='markers',
+                 marker=dict(size=28, color='rgb(0,240,0)'),
+
+                 hoverinfo='text')
